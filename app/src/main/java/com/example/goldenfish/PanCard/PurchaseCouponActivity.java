e173fe37ee0974de409e517e09db7861b1b4c027
AMan dev <gupta.amanknp@gmail.com>
2021-10-23 19:12:24 +0530

    pan ui

---
+++ app/src/main/java/com/example/goldenfish/PanCard/PurchaseCouponActivity.java
@@ -0,0 +1,20 @@
+package com.example.goldenfish.PanCard;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+import com.example.goldenfish.R;
+
+public class PurchaseCouponActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_purchase_coupon);
+    }
+
+    public void ProceedCouponPuchase(View view) {
+    }
+}
