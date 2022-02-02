c66252416fbb153a6ee3b5cfde31d70aed4a4394
AMan dev <gupta.amanknp@gmail.com>
2022-02-02 13:04:51 +0530

    code latest

---
+++ app/src/main/java/com/business/goldenfish/AddFund/AddFundActivity.java
@@ -0,0 +1,16 @@
+package com.business.goldenfish.AddFund;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.business.goldenfish.R;
+
+public class AddFundActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_fund);
+    }
+}
